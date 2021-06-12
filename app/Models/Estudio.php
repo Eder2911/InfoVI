d1e825eb41a614c01c08316c3bbbd8f2d306bf89
Eder2911 <exer_dodo@comunidad.unam.mx>
2021-06-11 22:44:25 -0500

    1er commit

---
+++ app/Models/Estudio.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Estudio extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'tipo', 
+        'fechaRealizacion',
+        'asistencia', 
+        'fechaEntrega', 
+        'fechaProximo', 
+        'fechaRevision', 
+        'resultado', 
+        'documento'];
+
+}
